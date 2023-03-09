6a59c92e445f6ac3da6fdc4a7bbfcdb6d688d861
orlapez <orlapez@gmail.com>
2023-03-09 14:57:03 -0500

    Commit Initial

---
+++ StoresG1.API/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using StoresG1.API.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+//Inyección de dependencias del servicio SQl Server
+builder.Services.AddDbContext<DataContext > (x => x.UseSqlServer("name=DefaultConnection"));
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ StoresG1.Shared/Entities/Country.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoresG1.Shared.Entities
+{
+    public class Country
+    {
+
+        public int Id { get; set; }
+
+        [Display(Name = "País")]  //{0}
+        [MaxLength(100, ErrorMessage = "Cuidado el campo {0} no permite más de {1} caracteres ")]  //{1}
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Name { get; set; } = null;
+
+
+    }
+}
